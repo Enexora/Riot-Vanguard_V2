0c80c174c004c6f25e579c5eafd1094422009f7c
Enexora <therealextrapt@gmail.com>
2021-12-24 04:30:53 -0500

    reorganized and touched up some, added framestagenotify hook

--- dllmain.cpp
+++ dllmain.cpp
@@ -7,8 +7,8 @@ DWORD WINAPI bhop(HMODULE hModule) {
     client = (DWORD)GetModuleHandle("client.dll");
     engine = (DWORD)GetModuleHandle("engine.dll");
     vguimatsurface = (DWORD)GetModuleHandle("vguimatsurface.dll");
-    finishDrawing = (tFinishDrawing)(vguimatsurface + 0xD560);
-    startDrawing = (tStartDrawing)(vguimatsurface + 0xD380);
+    finishDrawing = (tFinishDrawing)(pattern_scan("vguimatsurface.dll", "8B 0D ? ? ? ? 56 C6 05"));
+    startDrawing = (tStartDrawing)pattern_scan("vguimatsurface.dll", "55 8B EC 83 E4 C0 83 EC 38");
     clientMode = **(ClientModeShared***)((*(uintptr_t**)(pClientDLL))[10] + 0x5);
     input = *(CInput**)((*(DWORD**)pClientDLL)[16] + 1);
     Globals = **(CGlobals***)((*(DWORD**)pClientDLL)[11] + 0xA);
@@ -28,15 +28,18 @@ DWORD WINAPI bhop(HMODULE hModule) {
     VirtualProtect(*(void**)surface, 0xA00, PAGE_EXECUTE_READWRITE, &ulOldProtect[4]);
     fCreateMove = (tCreateMove)*(DWORD*)(vtable + 24*sizeof(void*));
     fLoadSkybox = (tLoadSkybox)(engine + 0x131720);
+    fFrameStageNotify = *(tFrameStageNotify*)(*(DWORD*)(pClientDLL) + 37*sizeof(void*));
     fPaint = (tPaint)*(DWORD*)((*(DWORD*)engineVGui) + (14 * sizeof(void*)));
     fOverrideView = (tOverrideView)*(DWORD*)(vtable + 18*sizeof(void*));
     fLockCursor = (tLockCursor)*(DWORD*)((*(DWORD*)surface) + 67 * sizeof(void*));
     *(DWORD*)((*(DWORD*)surface) + 67 * sizeof(void*)) = (DWORD)&hkLockCursor;
     *(DWORD*)((*(DWORD*)engineVGui) + (14 * sizeof(void*))) = (DWORD)&hkPaint;
+    *(DWORD*)(*(DWORD*)(pClientDLL)+ 37*sizeof(void*)) = (DWORD)&hkFrameStageNotify;
     *(DWORD*)(vtable + 24*sizeof(void*)) = (DWORD)&hkCreateMove;
     *(DWORD*)(vtable + 18*sizeof(void*)) = (DWORD)&hkOverrideView;
     EngineClient->ClientCmdUnrestricted("playvol ambient/flamenco.wav .25");
     while (!GetAsyncKeyState(VK_END)){
+        localPlayer = *(Player**)(client + dwLocalPlayer);
         toggle(bAA, VK_NUMPAD5);
         toggle(bBhop, VK_NUMPAD1);
         toggle(bAimbot, VK_NUMPAD4);
@@ -47,46 +50,44 @@ DWORD WINAPI bhop(HMODULE hModule) {
             toggleAll();
         }
         DWORD glowObj = *(DWORD*)(client + dwGlowObjectManager);
-        localPlayer = *(DWORD*)(client + dwLocalPlayer);
-        plocalPlayer = *(Player**)(client + dwLocalPlayer);
         if (!EngineClient->IsInGame()) continue;
         if (localPlayer == NULL) continue;
         if (EngineClient->GetNetChannelInfo() != netchan) {
             netchan = EngineClient->GetNetChannelInfo();
         }
-        for (int i = 0; i < 64; i++) {
-            Player* ent = *(Player**)(client + dwEntityList + i * 0x10);
+        for (int i = 0; i < 48; i++) {
+            Player* ent = (Player*)EntityList->GetClientEntity(i);
                 if (ent == NULL) continue;
-                if (*(int*)(ent + m_iTeamNum) != *(int*)(localPlayer + m_iTeamNum)) *(bool*)(ent + m_bSpotted) = true;
-                //if (ent != plocalPlayer) ent->m_angEyeAngles()->yaw = ent->m_flLowerBodyYawTarget();
-                if(bEsp){
-                int glowIndex = *(int*)(ent + m_iGlowIndex);
-                if (*(int*)(ent + m_iTeamNum) != *(int*)(localPlayer + m_iTeamNum)) {
-                    *(float*)(glowObj + glowIndex * 0x38 + 0x8) = 1.f;
-                    *(float*)(glowObj + glowIndex * 0x38 + 0xC) = 0.15f;
-                    *(float*)(glowObj + glowIndex * 0x38 + 0x10) = (*(int*)(ent + m_iHealth) / 200.f);
-                    *(float*)(glowObj + glowIndex * 0x38 + 0x14) = 1.f;
-                }
-                else
+                if (ent->m_iTeamNum() != localPlayer->m_iTeamNum()) *(bool*)(ent + m_bSpotted) = true;
+                if (*(float*)((DWORD)localPlayer + m_flFlashDuration) > 0)
                 {
-                    *(float*)(glowObj + glowIndex * 0x38 + 0x8) = 0.f;
-                    *(float*)(glowObj + glowIndex * 0x38 + 0xC) = 0.166f;
-                    *(float*)(glowObj + glowIndex * 0x38 + 0x10) = 1.f;
-                    *(float*)(glowObj + glowIndex * 0x38 + 0x14) = .6f;
+                    *(float*)((DWORD)localPlayer + m_flFlashDuration) = 0;
                 }
-                *(int*)(glowObj + glowIndex * 0x38 + 0x28) = 1;
-                *(int*)(glowObj + glowIndex * 0x38 + 0x29) = 0;
+                if(bEsp && ent->m_iHealth()){
+                    int glowIndex = *(int*)((DWORD)ent + m_iGlowIndex);
+                    if (ent->m_iTeamNum() != localPlayer->m_iTeamNum()) {
+                        *(float*)(glowObj + glowIndex * 0x38 + 0x8) = 1.f;
+                        *(float*)(glowObj + glowIndex * 0x38 + 0xC) = 0.15f;
+                        *(float*)(glowObj + glowIndex * 0x38 + 0x10) = (ent->m_iHealth() / 200.f);
+                        *(float*)(glowObj + glowIndex * 0x38 + 0x14) = 1.f;
+                    }
+                    else
+                    {
+                        *(float*)(glowObj + glowIndex * 0x38 + 0x8) = 0.f;
+                        *(float*)(glowObj + glowIndex * 0x38 + 0xC) = 0.166f;
+                        *(float*)(glowObj + glowIndex * 0x38 + 0x10) = 1.f;
+                        *(float*)(glowObj + glowIndex * 0x38 + 0x14) = .6f;
+                    }
+                    *(int*)(glowObj + glowIndex * 0x38 + 0x28) = 1;
+                    *(int*)(glowObj + glowIndex * 0x38 + 0x29) = 0;
             }
-        }
-        if (*(float*)(localPlayer + m_flFlashDuration) > 0)
-        {
-            *(float*)(localPlayer + m_flFlashDuration) = 0;
         }
         Sleep(1);
     }
     toggleAll();
     *SendPacket = true;
     *(DWORD*)((*(DWORD*)engineVGui) + (14 * sizeof(void*))) = (DWORD)fPaint;
+    *(DWORD*)(*(DWORD*)(pClientDLL)+37 * sizeof(void*)) = (DWORD)fFrameStageNotify;
     *(DWORD*)((*(DWORD*)surface) + 67 * sizeof(void*)) = (DWORD)fLockCursor;
     *(DWORD*)(vtable + 24 * sizeof(void*)) = (DWORD)fCreateMove;
     *(DWORD*)(vtable + 18 * sizeof(void*)) = (DWORD)fOverrideView;
